b537c472fa112e5741b1b55dfb1022c4740e430e
ShuEugene <shueugene@hotmail.com>
2022-09-30 14:45:34 +0300

    HW9 initial.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//  Домашние задания к Уроку 9 «Объекты и Классы»
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        task1();
+
+    }
+
+    static void task1() {
+
+    }
+}
